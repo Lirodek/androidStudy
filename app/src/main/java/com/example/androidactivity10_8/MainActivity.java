3548808ecf5b7517b3e7293d897db213209c7bf6
Lirodek <Lirodek@gmail.com>
2022-04-11 19:32:24 +0900

    git LogCat

---
+++ app/src/main/java/com/example/androidactivity10_8/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.androidactivity10_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button callBtn, endBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        callBtn = (Button) findViewById(R.id.callBtn);
+        endBtn = (Button) findViewById(R.id.endBtn);
+
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("tel:010-9313-3895");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+        endBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        android.util.Log.i("액티비티 테스트", "onDestroy()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        android.util.Log.i("액티비티 테스트", "onPause()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        android.util.Log.i("액티비티 테스트", "onRestart()");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        android.util.Log.i("액티비티 테스트", "onPostResume()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        android.util.Log.i("액티비티 테스트", "onStop()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        android.util.Log.i("액티비티 테스트", "onStart()");
+    }
+}
